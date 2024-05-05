a99f8330a19001ae7bcb27c83966942e391e0d97
sam <sam@ora.ma>
2024-05-05 20:25:57 +0100

    dont ignor ts errors

--- next.config.js
+++ next.config.js
@@ -9,13 +9,13 @@ const nextConfig = {
       360, 414, 512, 640, 750, 828, 1080, 1200, 1536, 1920, 2048, 3840,
     ],
   },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
+  // typescript: {
+  //   // !! WARN !!
+  //   // Dangerously allow production builds to successfully complete even if
+  //   // your project has type errors.
+  //   // !! WARN !!
+  //   ignoreBuildErrors: true,
+  // },
 }
 
 module.exports = withContentlayer(nextConfig)
