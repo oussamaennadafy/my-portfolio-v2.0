663b015885924ceef9b0a60c0b62200ff7f82f02
oussama <oussama@ennadafy@gmail.com>
2024-05-14 16:46:53 +0100

    my bad not yet

--- public/assets/scripts/syncTheme.js
+++ public/assets/scripts/syncTheme.js
@@ -1,11 +1,10 @@
 const getBrowserDefaultTheme = () => {
   if (window?.matchMedia && window?.matchMedia?.('(prefers-color-scheme: dark)')?.matches) return "dark";
   if (window?.matchMedia && window?.matchMedia?.('(prefers-color-scheme: light)')?.matches) return "light";
-  return "dark";
 };
 
-const theme = JSON.parse(window.localStorage?.getItem("theme")) || getBrowserDefaultTheme();
+const theme = JSON.parse(window.localStorage?.getItem("theme")).state.theme || getBrowserDefaultTheme();
 
 const htmlElement = document.getElementsByTagName("html").item(0);
 
-htmlElement.classList.add(theme.state.theme);
+htmlElement.classList.add(theme);
