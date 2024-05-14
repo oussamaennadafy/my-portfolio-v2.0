86e0e8bbda4a2bf1c6b23481a9778f65af405ab5
oussama <oussama@ennadafy@gmail.com>
2024-05-14 16:31:32 +0100

    switch to presist

--- src/store/useTheme.ts
+++ src/store/useTheme.ts
@@ -2,6 +2,7 @@
 
 import getBrowserDefaultTheme from '@/utils/getBrowserDefaultTheme';
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 type themeType = "dark" | "light";
 
@@ -12,43 +13,65 @@ interface themeStoreType {
 };
 
 const getInitialUserTheme = (): themeType => {
-  // const theme: any = JSON.parse(localStorage?.getItem("theme")) || getBrowserDefaultTheme();
-  const theme: any = getBrowserDefaultTheme()
+  const theme: any = JSON.parse(localStorage?.getItem("theme")) || getBrowserDefaultTheme();
   return theme;
 };
 
-export const useTheme = create<themeStoreType>((set, get) => ({
-  theme: getInitialUserTheme(),
-  setTheme: (theme: themeType) => {
-    // get html element
-    const htmlElement = document.getElementsByTagName("html").item(0);
+export const useTheme = create(
+  persist<themeStoreType>(
+    (set, get) => ({
+      theme: getInitialUserTheme(),
+      setTheme: (theme) => {
+        // get html element
+        const htmlElement = document.getElementsByTagName("html").item(0);
 
-    // re-paint the ui for the user
-    theme === "light" ? htmlElement.classList.remove("dark") : htmlElement.classList.add("dark");
+        // re-paint the ui for the user
+        theme === "light" ? htmlElement.classList.remove("dark") : htmlElement.classList.add("dark");
 
-    // update store data
-    set({ theme });
+        // update store data
+        set({ theme });
+      },
+      toggleTheme: () => {
+        // get html element
+        const htmlElement = document.getElementsByTagName("html").item(0);
 
-    // sync localStorage with store data
-    localStorage.setItem("theme", JSON.stringify(theme));
-  },
-  toggleTheme: () => {
-    // get html element
-    const htmlElement = document.getElementsByTagName("html").item(0);
+        // re-paint the ui for the user
+        htmlElement.classList.toggle("dark");
 
-    // re-paint the ui for the user
-    htmlElement.classList.toggle("dark");
+        // get current theme
+        const currentTheme = get().theme;
 
-    // get current theme
-    const currentTheme = get().theme;
+        // derived toggled theme
+        const newTheme = currentTheme === "light" ? "dark" : "light"
 
-    // derived toggled theme
-    const newTheme = currentTheme === "light" ? "dark" : "light"
+        // update store data
+        set({ theme: newTheme });
+      },
+    }),
+    {
+      name: 'theme-storage', // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
+    },
+  ),
+)
+// export const useThemee = create<themeStoreType>((set, get) => ({
+//   theme: getInitialUserTheme(),
+//   setTheme: (theme: themeType) => {
+//     // get html element
+//     const htmlElement = document.getElementsByTagName("html").item(0);
 
-    // update store data
-    set({ theme: newTheme });
+//     // re-paint the ui for the user
+//     theme === "light" ? htmlElement.classList.remove("dark") : htmlElement.classList.add("dark");
 
-     // sync localStorage with store data
-     localStorage.setItem("theme", JSON.stringify(newTheme));
-  }
-}));
+//     // update store data
+//     set({ theme });
+
+//     // sync localStorage with store data
+//     localStorage.setItem("theme", JSON.stringify(theme));
+//   },
+//   toggleTheme: () => {
+
+//     // sync localStorage with store data
+//     localStorage.setItem("theme", JSON.stringify(newTheme));
+//   }
+// }));
