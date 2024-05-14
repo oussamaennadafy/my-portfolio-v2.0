39a43943d21cbbedb8b302a8372218bea86a675a
oussama <oussama@ennadafy@gmail.com>
2024-05-14 15:57:21 +0100

    next font error

--- public/assets/scripts/syncTheme.js
+++ public/assets/scripts/syncTheme.js
@@ -3,7 +3,6 @@ const getBrowserDefaultTheme = () => {
   if (window?.matchMedia && window?.matchMedia?.('(prefers-color-scheme: light)')?.matches) return "light";
   return "dark";
 }
-
 const theme = JSON.parse(window.localStorage?.getItem("theme")) || getBrowserDefaultTheme();
 
 const htmlElement = document.getElementsByTagName("html").item(0);
