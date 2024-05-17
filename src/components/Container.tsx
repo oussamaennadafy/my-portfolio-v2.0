0da70a1ba1fb9f242efb8e8d4c88a83b52d98555
oussama <oussama@ennadafy@gmail.com>
2024-05-17 23:33:40 +0100

    ...

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,15 +4,18 @@ import { ReactNode } from 'react'
 type ContainerProps = {
   className?: string,
   children: ReactNode | ReactNode[],
+  // ref: React.RefObject<HTMLDivElement>,
+  ref?: (node?: Element) => void,
 }
 
-export function Container({ className, ...props }: ContainerProps) {
+export function Container({ className, ref, ...props }: ContainerProps) {
   return (
     <div
       className={clsx(
         'mx-auto max-w-screen-xl px-5 sm:px-6 lg:px-8',
         className
       )}
+      ref={ref}
       {...props}
     />
   )
